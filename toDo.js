a99e719b63cfd061fc8b167628462a8db505cd2a
Ypass <Apasihe2002@gmail.com>
2021-07-08 11:52:07 +0300

    toDo List

---
+++ toDo.js
@@ -0,0 +1,91 @@
+
+let addMessage = document.querySelector('.message');
+let addButton = document.querySelector('.add');
+let todo = document.querySelector('.todo');
+let todoList = [];
+
+if(localStorage.getItem('todo')) {
+    todoList = JSON.parse(localStorage.getItem('todo'));
+    displayMessage()
+}
+
+addButton.addEventListener('click', function() {
+    if(!addMessage.value) return;
+    let newToDo = {
+        todo: addMessage.value,
+        checked: false,
+        important: false
+    }
+
+    todoList.push(newToDo);
+    displayMessage()
+    localStorage.setItem('todo', JSON.stringify(todoList));
+    addMessage.value = '';
+})
+
+function displayMessage() {
+    let displayMessage = '';
+    if(todoList.length === 0) todo.innerHTML = '';
+    todoList.forEach(function(item, i) {
+        displayMessage += `
+        <li>
+           <input type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}>
+           <label for='item_${i}' class='${item.important ? 'important' : '' }'>${item.todo}</label>
+        </li>
+        `;
+
+        todo.innerHTML = displayMessage;
+    });
+
+};
+
+todo.addEventListener('change', function(event) {
+    let idInput = event.target.getAttribute('id');
+    let forLabel = todo.querySelector('[for='+ idInput +']');
+    let valueLabel = forLabel.textContent;
+//    console.log(valueLabel)
+
+
+    todoList.forEach(function (item) {
+        if (item.todo === valueLabel) {
+            item.checked = !item.checked;
+            localStorage.setItem('todo', JSON.stringify(todoList));
+        }
+    })
+})
+
+    todo.addEventListener('contextmenu', function(event) {
+        event.preventDefault();
+
+        todoList.forEach(function(item, i) {
+            if(item.todo === event.target.innerHTML) {
+                if(event.ctrlKey || event.metaKey) {
+                    todoList.splice(i, 1);
+                } else {
+                    item.important = !item.important;
+                }
+
+                localStorage.setItem('todo', JSON.stringify(todoList));
+                displayMessage()
+            }
+        })
+    })
+
+
+/* научился использовать LocalStorage ( сохранять на удаленном сервере, Юольше интересных проверок, постоянное использование циклов ) */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
